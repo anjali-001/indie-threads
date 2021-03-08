23c00eea0701489275aaad38f131aecb2a396b89
anjali-001 <anjali001.as@gmail.com>
2021-03-08 19:21:59 +0530

    sreach bar animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Nav, Navbar} from 'react-bootstrap'
 import FeatherIcon from 'feather-icons-react';
 import './Navbar.css'
 import logo from '../../assets/logo.svg'
 function NavHeader() {
+  const [toggle,setToggle] = useState(false);
+
     return (
         <div className="navHeader navHeader__style">
   <Navbar className="custom-container">
@@ -13,7 +15,10 @@ function NavHeader() {
       <Nav.Link className ="navHeader__item" href="#features">Explore</Nav.Link>
       <Nav.Link className ="navHeader__item" href="#pricing">Threads</Nav.Link>
     </Nav>
-    <FeatherIcon className="navHeader__search navHeader__icon" icon="search"/>
+    <div className="navHeader__searchBorder">
+    {toggle? <input placeholder="Search all games" type="text" className="navHeader__input"/>: null }
+    <FeatherIcon className="navHeader__search navHeader__icon" icon="search"  onClick={() => setToggle(!toggle)}/>
+    </div>
     <FeatherIcon className="navHeader__user navHeader__icon" icon="user"/>
   </Navbar>
 
