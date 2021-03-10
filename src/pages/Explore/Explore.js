c925da72a04c023db3e38e918bafb09aff84571e
skeshavaa <s.keshavaa@gmail.com>
2021-03-10 14:43:59 -0500

    added firebase function to get posts, working on expore cards now

--- src/pages/Explore/Explore.js
+++ src/pages/Explore/Explore.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Explore.css'
+import getPosts from '../../constants/fire-functions/getPosts'
 
 const Explore = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        async function getData(){
+            const response = await getPosts();
+            setData(response)
+        }
+        getData();
+    }, [])
+
+    if (data == []){
+        return( 
+        <div className="explore-container">
+            <h1>sdf</h1>
+        </div>
+        )
+    }
     return (
-        <>
-            <p>Explore Page</p>
-        </>
+        <div className="explore-container">
+            {data.map((doc) => {
+                return(
+                    <h1>sdf</h1>
+                )
+            })}
+        </div>
     )
 }
 
