46725cd7647530b38845c2f1eaae69d43082456f
skeshavaa <s.keshavaa@gmail.com>
2021-03-10 19:29:30 -0500

    finished filters

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -4,11 +4,22 @@ import './Filter.css'
 import filter_icon from '../../assets/filter.png'
 
 const Filter = () => {
+
+    const examples = ["Horror", "Comedy", "Action", "Adventure", "Thrilling"]
+
+
     return(
         <div className="filter-container">
             <div className="filter-heading">
                 <img className="filter-img" src={filter_icon}/>
                 <h1 className="filter-title">Filters</h1>
+            </div>
+            <div className="all-filter">
+                {examples.map((genre) => {
+                    return(
+                        <a className="filter-genre">{genre}</a>
+                    )
+                })}
             </div>
         </div>
     )
