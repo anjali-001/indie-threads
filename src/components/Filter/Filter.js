77722bc34177a0ba11dfdc97b8c671099e23824c
anjali-001 <anjali001.as@gmail.com>
2021-03-15 00:23:17 +0530

    filter by genre added

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,12 +1,32 @@
-import react from 'react'
+import React, {useContext} from 'react'
 import './Filter.css'
+import { ExploreContext } from "../../context/ExploreContext";
+
 
 import filter_icon from '../../assets/filter.png'
 
 const Filter = () => {
 
+    const {data,setExpData} = useContext(ExploreContext);
+
     const examples = ["Horror", "Comedy", "Action", "Adventure", "Thrilling"]
 
+    const filterGenre = (val) => {
+
+        console.log(val);
+        console.log(data);
+        let arr = [];
+        data.filter((item) => {
+          if(item.genre.filter(post => 
+            {
+              if(post.toLowerCase().includes(val.toLowerCase()))
+              arr.push(item);
+            }
+          ));
+        });
+        console.log(arr)
+        setExpData(arr)
+    }
 
     return(
         <div className="filter-container">
@@ -17,7 +37,7 @@ const Filter = () => {
             <div className="all-filter">
                 {examples.map((genre) => {
                     return(
-                        <a className="filter-genre">{genre}</a>
+                        <div className="filter-genre" onClick={() => filterGenre(genre)}>{genre}</div>
                     )
                 })}
             </div>
